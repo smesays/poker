ae7adc43f5cf23293336c591f73adcebf333170a
smesays <smesays@users.noreply.github.com>
2018-02-22 11:23:57 -0600

    added comments for deck.py

--- g/deck.py
+++ g/deck.py
@@ -1,3 +1,7 @@
+# Code      : deck.py
+# Written by: LIU, Jiun Ee (G)
+# Purpose   : This class is used to represent a deck of 52 cards
+
 import time
 import numpy.random as random 
 
@@ -7,25 +11,21 @@ class Deck():
         self.deck=[]
         for i in range(1,53):
             self.deck.append(Card(i))
-#        self.shuffle(random.choice([4,5,6,7,8,9,10,11,42,25,476,5,58])) # change this later to timestamp based
-#        self.shuffle(random.choice(range(10000)))
         self.shuffle(int(time.time()*random.uniform(0,1)/10000))
 
     def shuffle(self,seed): # shuffle the deck by providing a seed number
         random.seed(seed)
         random.shuffle(self.deck)
         
+    def deal1(self): # deal next card
+        self.counter += 1
+        return self.deck[self.counter]
+        
+    def deal3(self): # deal next 3 cards (for flop)
+        return self.deal1(), self.deal1(), self.deal1()
+
     def __str__(self):
         temp=''
         for i in self.deck:
             temp = temp + i.show() + ' '
         return "%s" % temp
-# does declaring __iter__ and next makes it generic for other codes because it's consistent with regular container?
-#    def __iter__(self):
-#        return self
-    def deal1(self): # deal next card
-        self.counter += 1
-        return self.deck[self.counter]
-        
-    def deal3(self): # deal flop
-        return self.deal1(), self.deal1(), self.deal1()
