d3d3752e33c018f6718ae9cc1e86fec66cfdcf41
smesays <smesays@users.noreply.github.com>
2018-02-22 22:28:20 -0600

    added comments for hand_prob.py

--- g/hand_prob.py
+++ g/hand_prob.py
@@ -1,60 +1,24 @@
+# Code      : hand_prob.py
+# Written by: LIU, Jiun Ee (G)
+# Purpose   : Calculate winning probabilities of a 5-card poker hand
+
 from collections import defaultdict
 import pickle 
+
+HAND_COMBO = 2598960	# 52 pick 5
+
 '''
-pkl_file = open('hand_dict.pkl', 'rb') 
+pkl_file = open('hand_name_dict.pkl', 'rb') 
 hand_map = pickle.load(pkl_file) 
 pkl_file.close()
 print 'Hand map length', len(hand_map)
-'''
 
-HAND_COMBO = 2598960
-'''
 if len(hand_map) != HAND_COMBO:
 	print 'Hand map dictionary is not correct'
 '''
-'''
-pkl_file = open('hand_count.pkl', 'rb') 
-hand_cnt = pickle.load(pkl_file) 
-pkl_file.close()
- 
-print 'Unique hand count', len(hand_cnt)
 
-list=[] 
-for key, value in hand_cnt.iteritems():
-	list.append((key,value)) 
-
-cum_cnt = 0
-hand_prob = defaultdict(float)
-hand_prob_list = []
-for i in sorted(list):
-	cum_cnt += i[1]
-	hand_prob[i[0]] = cum_cnt * 1.0 / HAND_COMBO
-	hand_prob_list.append((i[0], cum_cnt * 1.0 / HAND_COMBO))
-
-for i in hand_prob_list:
-	print '%.3f %s' % (i[1], i[0]) 
-'''
-# construct win probability for the 10 hand types
-pkl_file = open('name_count.pkl', 'rb')
-name_cnt = pickle.load(pkl_file)
-pkl_file.close()
-
-name_cnt_list=[]
-name_prob_dict = defaultdict(float)
-for key, value in name_cnt.iteritems():
-    name_cnt_list.append((key, value))
-for i in sorted(name_cnt_list):
-    name_prob_dict[i[0]] = 1 - i[1] * 1.0 / HAND_COMBO
-    print '%50s %.4f' % (i[0], 1 - i[1] * 1.0 / HAND_COMBO)
-
-# write dict to file
-output = open('name_prob_dict.pkl', 'wb')
-pickle.dump(name_prob_dict, output)
-output.close()
-
-'''
 # construct win probability for the 10 hand types 
-pkl_file = open('type_count.pkl', 'rb')
+pkl_file = open('type_count_dict.pkl', 'rb')
 type_cnt = pickle.load(pkl_file)
 pkl_file.close()
 
@@ -72,4 +36,23 @@ output.close()
 
 for key, value in type_prob_dict.iteritems():
     print '%20s %.2f' % (key, value)
+
+'''
+# construct win probability for the unique hand names
+pkl_file = open('name_count_dict.pkl', 'rb')
+name_cnt = pickle.load(pkl_file)
+pkl_file.close()
+
+name_cnt_list=[]
+name_prob_dict = defaultdict(float)
+for key, value in name_cnt.iteritems():
+    name_cnt_list.append((key, value))
+for i in sorted(name_cnt_list):
+    name_prob_dict[i[0]] = 1 - i[1] * 1.0 / HAND_COMBO
+    print '%50s %.4f' % (i[0], 1 - i[1] * 1.0 / HAND_COMBO)
+
+# write dict to file
+output = open('name_prob_dict.pkl', 'wb')
+pickle.dump(name_prob_dict, output)
+output.close()
 '''
